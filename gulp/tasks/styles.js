6d757ae34fe2dee8ae95f2394f08da5e83853263
lucian95 <luciancondrea95@gmail.com>
2017-12-22 17:30:55 -0500

    Refactor gulpfile

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const importCss = require('postcss-import');
+
+gulp.task('styles', function () {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([importCss, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+
+gulp.task('injectCss', ['styles'], function () {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('injectCss');
+  });
+
+  watch('./app/index.html', function () {
+    browserSync.reload();
+  });
+});
