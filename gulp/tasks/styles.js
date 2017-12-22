cf1c7dd89bf8b53e80582285a15865a65339a01a
lucian95 <luciancondrea95@gmail.com>
2017-12-22 17:40:02 -0500

    Add error handling to gulp styles task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,9 @@ const importCss = require('postcss-import');
 gulp.task('styles', function () {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([importCss, cssvars, nested, autoprefixer]))
+    .on('error', function (error) {
+      console.log(error.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
