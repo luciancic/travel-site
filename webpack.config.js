446ea9c9369219fa94397dfbc827c9854034a817
lucian95 <luciancondrea95@gmail.com>
2018-01-08 11:48:54 -0500

    Add babel to webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     path: __dirname + '/app/temp/scripts',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
   }
 };
