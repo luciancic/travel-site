084b6b8b9b2507746c477994e9ed4a62dcdc54f3
lucian95 <luciancondrea95@gmail.com>
2018-01-10 19:46:35 -0500

    Reveal features and testimonials on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,4 @@
+require('./modules/RevealOnScroll');
 const MobileMenu = require('./modules/MobileMenu');
 
 const mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,5 @@
+const ScrollReveal = require('scrollreveal');
+console.log(ScrollReveal);
+window.sr = ScrollReveal();
+
+sr.reveal('.item-to-reveal', {duration: 700, easing: 'ease-out'});
